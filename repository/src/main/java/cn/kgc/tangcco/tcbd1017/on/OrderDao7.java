1e7ccb3cc17c1f8610cd5412aef564b5fd8655fa
hubin20191213 <Administrator@kgc_hubin>
2019-12-13 18:49:32 +0800

    hubin20191213上传到主版本库18点49

---
+++ repository/src/main/java/cn/kgc/tangcco/tcbd1017/on/OrderDao7.java
@@ -0,0 +1,31 @@
+package cn.kgc.tangcco.tcbd1017.on;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import cn.kgc.tangcco.tcbd1017.on.pojo.Order;
+/**
+ * 
+ * @author 廖斌
+ *
+ */
+
+public interface OrderDao7 {
+		/**
+		 * @return List<Order>
+		 * 	查询订单 
+		 * @throws SQLException 
+		 */
+		public List<Order> selectByOrder(Map<String,Object> map) throws SQLException;
+		/**
+		 * @return int
+		 * 	修改订单，修改订单
+		 */
+		public int updateByOrder( Map<String,Object> map )throws SQLException;
+		/**
+		 * @return int
+		 * 	新增订单
+		 */
+		public int insertByOrder(Map<String,Object> map)throws SQLException;
+}
