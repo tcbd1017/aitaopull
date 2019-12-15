201dac8378b5e9267864177a651fa6d1613199f6
许佳瑞20191215 <Administrator@LAPTOP-GRQ3U5DL>
2019-12-15 18:57:02 +0800

    许佳瑞20191215人事部的dao层代码和service层代码和action的框架

---
+++ repository/src/main/java/cn/kgc/tangcco/tcbd1017/on/system/PersonnelMattersDao.java
@@ -0,0 +1,26 @@
+package cn.kgc.tangcco.tcbd1017.on.system;
+
+import java.util.Map;
+
+/**
+* @author 许佳瑞
+* @version 创建时间：2019年12月14日 下午7:33:02
+* @edition 1.0
+* @Description 类描述
+*/
+
+public interface PersonnelMattersDao {
+		
+	/**
+	 * 
+	 * 办理离职
+	 */
+	int delecteandupdateEmp(Map<String, Object> map);
+	
+	/**
+	 * 分配员工部门
+	 * 
+	 */
+	int updateEmpdept(Map<String, Object> map);
+}
+	
---
+++ service/src/main/java/cn/kgc/tangcco/tcbd1017/on/system/PersonnelMattersService.java
@@ -0,0 +1,27 @@
+package cn.kgc.tangcco.tcbd1017.on.system;
+
+import java.util.Map;
+
+/**
+* @author 许佳瑞
+* @version 创建时间：2019年12月15日 下午3:01:46
+* @edition 1.0
+* @Description 类描述
+*/
+public interface PersonnelMattersService {
+	
+	/**
+	 * 删除员工
+	 * @param map
+	 * @return
+	 */
+	Map<String, Object>removeEmpstates (Map<String, Object>map);
+	
+	/**
+	 * 根据权限修改员工部门
+	 * @param map
+	 * @return
+	 */
+	Map<String, Object>modifyEmpDept( Map<String, Object> map);
+	
+}
